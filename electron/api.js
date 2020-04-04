110a8d94c6aed2a7129d7361951fc44a73cf5817
mittsuyama <1462973856@qq.com>
2020-04-04 17:26:53 +0800

    new folder, fetch folder list

--- electron/api.js
+++ electron/api.js
@@ -1,10 +1,59 @@
 const { ipcMain } = require('electron');
+const path = require('path');
 const fs = require('fs');
 const uuid = require('uuid');
 
+const ROOT = 'data';
+
+const newFile = (fileName, content = "") => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fileName, content, function(error) {
+      if(error) {
+        console.log(error);
+        reject(error);
+      } else {
+        resolve(200);
+      }
+    });
+  })
+};
+
+const listFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path, function(error, data) {
+      if(error) {
+        reject(error);
+      } else {
+        resolve({
+          status: 200,
+          data: data,
+        });
+      }
+    });
+  })
+};
+
 module.exports = () => {
-  ipcMain.on('add-folder', (event, arg) => {
-    event.returnValue = uuid.v1();
+  ipcMain.on('add-folder', async(event, arg) => {
+    const newID = uuid.v1();
+    const nowTime = Date.now();
+    const conf = {
+      name: arg.name || 'new folder',
+      id: newID,
+      folder: arg.folder || 0,
+      create_time: nowTime,
+      update_time: nowTime,
+    };
+    const result = await newFile(path.join(__dirname, `${ROOT}/folder/${newID}.json`), JSON.stringify(conf));
+    if(result === 200) {
+      event.returnValue = {
+        status: 200,
+      };
+    }
   });
+
+  ipcMain.on('fetch-folders-structure', async (event, arg) => {
+    event.returnValue = await listFile(path.join(__dirname, `${ROOT}/folder`));
+  })
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import './index.less';
 // @ts-ignore
 const { ipcRenderer } = window.electron;
 
 export default () => {
+  // @ts-ignore
+  const { register, handleSubmit } = useForm();
   const [info, setInfo] = useState([]);
-  const [addFolderInput, setAddFolderInput] = useState("");
 
   useEffect(() => {
+    fetchFolders();
     setInfo([]);
   }, []);
 
@@ -15,15 +18,11 @@ export default () => {
     <div className="home-container">
       <div className="info-container">{infoRender(info)}</div>
       <div className="form-container">
-        <div className="item">
-          <input
-            type="text"
-            value={addFolderInput}
-            onChange={(event) => {
-              setAddFolderInput(event.target.value);
-            }}/>
-            <button onClick={() => addFolder(addFolderInput)}>ADD FOLDER</button>
-        </div>
+        <form onSubmit={handleSubmit(addFolder)} className="item">
+          <input type="text" name="name" ref={register}/>
+          <input type="text" name="folder" ref={register}/>
+          <input type="submit"/>
+        </form>
         <div className="item">new document</div>
         <div className="item">delete folder</div>
         <div className="item">delete document</div>
@@ -32,13 +31,20 @@ export default () => {
   );
 }
 
-const addFolder = (value: string) => {
+const addFolder = (value: any) => {
   const result = ipcRenderer.sendSync('add-folder', value);
-  console.log(result);
+  console.log(value);
 };
 
 const infoRender = (info: any) => {
    return (
      <div>files list</div>
    );
+};
+
+const fetchFolders = () => {
+  const result = ipcRenderer.sendSync('fetch-folders-structure');
+  if(result.status === 200) {
+    console.log(result.data);
+  }
 };
